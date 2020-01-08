c15fb6ade9229e8569cd2751cd391ba568f3983f
Drew Fitz-Maurice <drew.fitzmaurice@skookum.com>
2020-01-08 12:35:19 -0500

    tailwind init

--- firebase/website/src/main.js
+++ firebase/website/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
+// Styles
+import './assets/styles/index.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
