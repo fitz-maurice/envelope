ee15468d137f39a95499df96ba53dd2e1148d557
Colin <colin@fitz-maurice.com>
2020-01-08 15:23:18 -0500

    installing rn firebase & native-base

--- ios/envelope/AppDelegate.m
+++ ios/envelope/AppDelegate.m
@@ -11,6 +11,9 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+// Firebase
+@import Firebase;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -27,6 +30,12 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  // Firebase
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
   return YES;
 }
 
--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,15 @@
  *
  * @format
  */
-
+const blacklist = require('metro-config/src/defaults/blacklist');
 module.exports = {
+  resolver:{
+    blacklistRE: blacklist([
+      /firebase\/.*/,
+      /android\/.*/,
+      /ios\/.*/
+    ])
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
