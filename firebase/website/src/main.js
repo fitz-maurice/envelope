d8307501405d2dc8330a5e5418c12a937c01f37a
Drew Fitz-Maurice <drew@fitz-maurice.com>
2020-01-09 17:24:52 -0500

    firebase init

--- firebase/website/src/main.js
+++ firebase/website/src/main.js
@@ -2,11 +2,27 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import {firestorePlugin} from 'vuefire';
 
 // Styles
 import './assets/styles/index.css';
 
 Vue.config.productionTip = false;
+
+// Firebase
+Vue.use(firestorePlugin);
+
+// Auto require all single file components
+const files = require.context('./', true, /\.vue$/i);
+files.keys().map(key =>
+  Vue.component(
+    key
+      .split('/')
+      .pop()
+      .split('.')[0],
+    files(key).default,
+  ),
+);
 
 new Vue({
   router,
