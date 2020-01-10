aa2bb4c70754118695df58a3b2acd25ee10ffb15
Colin <colin@fitz-maurice.com>
2020-01-10 12:06:55 -0500

    wip

--- src/screens/app/Upload/Upload.js
+++ src/screens/app/Upload/Upload.js
@@ -2,6 +2,8 @@ import React, {useState, useCallback, useContext} from 'react';
 import {Alert} from 'react-native';
 import {WebView} from 'react-native-webview';
 import Icon from 'react-native-vector-icons/Feather';
+import storage from '@react-native-firebase/storage';
+import ImagePicker from 'react-native-image-crop-picker';
 import {Container, Content, Text, Header, Left, Right, Button, Body, Title} from 'native-base';
 
 import styles from './styles';
@@ -9,6 +11,33 @@ import AppContext from '../../../config/context';
 
 const Upload = ({navigation}) => {
   const context = useContext(AppContext);
+
+  const _takePicture = () => {
+    // ImagePicker.openCamera({
+    //   width: 300,
+    //   height: 400,
+    //   cropping: true,
+    // }).then(image => {
+    //   console.log(image);
+    // });
+
+    // ImagePicker.openPicker({
+    //   width: 300,
+    //   height: 400,
+    //   cropping: true
+    // }).then(image => {
+    //   console.log(image);
+    // });
+
+    // ImagePicker.openCropper({
+    //   path: '/private/var/mobile/Containers/Data/Application/F640393C-E1D2-4702-A470-26B49F6F899D/tmp/react-native-image-crop-picker/6F6FB312-1B4D-420D-8440-615C0668EBF7.jpg',
+    //   width: 200,
+    //   height: 200,
+    //   cropping: true,
+    // }).then(image => {
+    //   console.log(image);
+    // });
+  };
 
   /**
    * _cancel
@@ -47,6 +76,10 @@ const Upload = ({navigation}) => {
       {/* Content */}
       <Content padder>
         <Text>Upload Page</Text>
+
+        <Button block onPress={_takePicture}>
+          <Text>Take picture</Text>
+        </Button>
       </Content>
     </Container>
   );
