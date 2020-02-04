71c634833f7fccc12a232d9968a815ea87b958fe
Drew Fitz-Maurice <drew@fitz-maurice.com>
2020-02-04 02:43:14 -0500

    updates, edits, and deletes

--- app/services/card.js
+++ app/services/card.js
@@ -14,21 +14,56 @@ export default class CardService extends Base {
   async getCards() {
     let cards = [];
 
-    const querySnapshot = await firebase.firestore
+    const collection = await firebase.firestore
       .collection(`${this.uid}/account/cards`)
-      .get();
+      .orderBy('date', 'desc');
 
-    querySnapshot.docSnapshots.map(doc => {
-      const card = doc.data();
-      const temp = card.images.map(image =>
-        firebase.storage.getDownloadUrl({
-          remoteFullPath: `${this.uid}/${image}`,
-        }),
-      );
+    collection.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        // NEW CARDS ADDED
+        if (change.type === 'added') {
+          const card = { id: change.doc.id, ...change.doc.data() };
+          const temp = card.images.map(image =>
+            firebase.storage.getDownloadUrl({
+              remoteFullPath: `${this.uid}/${image}`,
+            }),
+          );
 
-      Promise.all(temp).then(result => {
-        card.images = result;
-        cards.push(card);
+          Promise.all(temp).then(result => {
+            card.images = result;
+            cards.push(card);
+          });
+        }
+
+        // CARDS THAT WERE EDITED
+        if (change.type === 'modified') {
+          const card = { id: change.doc.id, ...change.doc.data() };
+          const temp = card.images.map(image =>
+            firebase.storage.getDownloadUrl({
+              remoteFullPath: `${this.uid}/${image}`,
+            }),
+          );
+
+          const oldCard = cards.indexOf(cards.find(old => old.id === card.id));
+
+          Promise.all(temp).then(result => {
+            card.images = result;
+
+            if (~oldCard) {
+              cards.splice(oldCard, 1, card);
+            }
+          });
+        }
+
+        // DELETING A CARD
+        if (change.type === 'removed') {
+          const card = { id: change.doc.id, ...change.doc.data() };
+          const oldCard = cards.indexOf(cards.find(old => old.id === card.id));
+
+          if (~oldCard) {
+            cards.splice(oldCard, 1);
+          }
+        }
       });
     });
 
